1da64af557ece7b2e0ddd9b0ec2ea85ce7f110f0
Petr Chromec <petr.chromec@lmc.eu>
2018-10-23 17:09:25 +0200

    Test parameters

---
+++ tests/Entity/ParameterTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Lmc\ApiFilter\Entity;
+
+use Lmc\ApiFilter\AbstractTestCase;
+
+class ParameterTest extends AbstractTestCase
+{
+    /**
+     * @test
+     */
+    public function shouldCreateParameterWithDefaults(): void
+    {
+        throw new \Exception(sprintf('Method %s is not implemented yet.', __METHOD__));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateParameterWithAllValues(): void
+    {
+        throw new \Exception(sprintf('Method %s is not implemented yet.', __METHOD__));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateParameterByArray(): void
+    {
+        throw new \Exception(sprintf('Method %s is not implemented yet.', __METHOD__));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateParameterWithDefaultValueOnly(): void
+    {
+        throw new \Exception(sprintf('Method %s is not implemented yet.', __METHOD__));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotGetDefaultValueIfNotSet(): void
+    {
+        throw new \Exception(sprintf('Method %s is not implemented yet.', __METHOD__));
+    }
+}
