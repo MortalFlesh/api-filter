683e09815ae381296925c5a7a334119a44d9e867
Petr Chromec <petr.chromec@lmc.eu>
2018-11-20 14:50:04 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Parse functions in function parsers

---
+++ tests/Service/Parser/FunctionParser/ExplicitFunctionDefinitionParserTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Lmc\ApiFilter\Service\Parser\FunctionParser;
+
+/**
+ * @group unit
+ * @covers \Lmc\ApiFilter\Service\Parser\FunctionParser\ExplicitFunctionDefinitionParser
+ */
+class ExplicitFunctionDefinitionParserTest extends AbstractFunctionParserTestCase
+{
+    protected function setUp(): void
+    {
+        $this->parser = new ExplicitFunctionDefinitionParser($this->mockFilterFactory(), $this->initFunctions());
+    }
+
+    public function provideNotQueryParameters(): array
+    {
+        return self::CASE_EXPLICIT_FUNCTION_DEFINITION_BY_VALUES
+            + self::CASE_IMPLICIT_FUNCTION_DEFINITION_BY_VALUES
+            + self::CASE_EXPLICIT_FUNCTION_DEFINITION_BY_TUPLE
+            + self::CASE_IMPLICIT_FUNCTION_DEFINITION_BY_TUPLE;
+    }
+
+    public function provideParseableQueryParameters(): array
+    {
+        return self::CASE_EXPLICIT_FUNCTION_DEFINITION;
+    }
+}
